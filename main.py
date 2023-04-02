b811f477b8cf4c7d10469509c4f39383c5a881e6
jasper <49565505+wluxie@users.noreply.github.com>
2023-04-02 11:07:16 -0700

    -

--- main.py
+++ main.py
@@ -1,40 +1,22 @@
 from calibrate import *
 
-
-class OriveNEO:
-
-    def __init__(self, odrv):
-        self.odrv = odrive.find_any(serial_number=odrv)
-
-    def close_loop_control(self):
-        self.odrv.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-        self.odrv.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
-
-    def idle_state(self):
-        self.odrv.axis0.requested_state = AXIS_STATE_IDLE
-        self.odrv.axis1.requested_state = AXIS_STATE_IDLE
-
-    def set_motor_velocity(self, speed):
-        self.odrv.axis0.controller.input_vel = speed
-        self.odrv.axis1.controller.input_vel = speed
-
-
 def main():
     odrives = get_all_odrives()
     odrv = odrives[0]
     calibrate_all_motors(odrv)
 
-    neo = OriveNEO(odrv)
+    odrvControl = odrive.find_any(serial_number=odrv)
+
 
     while True:
-        print("Spinning")
-        neo.close_loop_control()
-        neo.set_motor_velocity(5)
-        time.sleep(5)
-        print("Sleeping")
-        neo.set_motor_velocity(0)
-        neo.idle_state()
-        time.sleep(5)
+        print("Boom")
+        odrvControl.axis0.controller.input_vel = 5
+        odrvControl.axis1.controller.input_vel = 5
+        odrvControl.axis0.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        odrvControl.axis1.requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL
+        time.sleep(3)
+        odrvControl.axis0.requested_state = AXIS_STATE_IDLE
+        odrvControl.axis1.requested_state = AXIS_STATE_IDLE
 
     
 
